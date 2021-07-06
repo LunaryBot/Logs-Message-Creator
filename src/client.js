0883348434193a3276b13deb4d1b3ac1ef7805df
JVOPINHO <joaoamoreterno1511@gmail.com>
2021-07-06 16:03:43 -0300

    Command Hnadler

--- src/client.js
+++ src/client.js
@@ -2,4 +2,7 @@ require("dotenv").config();
 const { Client } = require("discord.js");
 const client = new Client();
 
+require("./functions/quote");
+require("./handlers/commandHandler")(client);
+
 client.login(process.env.TOKEN);
